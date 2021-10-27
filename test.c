4ba20882d28f4d9e92e19b3468f607fe4026bfd3
Guillaume Morange <gmorange@e1r1p3.42.fr>
2021-10-27 18:30:22 +0200

    test

--- test.c
+++ test.c
@@ -1,261 +1,158 @@
-#include <math.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-
-typedef struct	s_back
-{
-	float	width;
-	float	height;
-	char	c;
-}				t_back;
-
-typedef struct	s_figure
-{
-	char	r;
-	float	x;
-	float	y;
-	float	width;
-	float	height;
-	char	c;
-}				t_figure;
 
 typedef struct	s_all
 {
-	FILE *file;
-	char **tab;
-	t_back back;
-	t_figure figure;
+	FILE	*file;
+	char	**back;
+	float	b_width;
+	float	b_height;
+	char	b_char;
+	char	s_r;
+	float	s_x;
+	float	s_y;
+	float	s_width;
+	float	s_height;
+	char	s_char;
 }				t_all;
 
 int		ft_strlen(char *str)
 {
-	int i;
-
-	i = 0;
+	int i = 0;
 	while (str[i])
 		i++;
 	return (i);
 }
 
-void	ft_putstr(char *str)
+int		ft_putstr(char *str)
 {
 	write(1, str, ft_strlen(str));
+	return(1);
 }
+
+void	destroy(t_all *all)
+{
+	int i = 0;
+
+	while (all->back[i])
+	{
+		free(all->back[i]);
+		all->back[i] = NULL;
+		i++;
+	}
+	free(all->back);
+	all->back = NULL;
+}
+
 void	print_tab(t_all *all)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (all->tab[i])
+	while (all->back[i])
 	{
-		ft_putstr(all->tab[i]);
+		ft_putstr(all->back[i]);
 		ft_putstr("\n");
 		i++;
 	}
 }
 
-void	free_tab(t_all *all)
+int		malloc_and_stock_back(t_all *all)
 {
-	int i;
-
-	i = 0;
-	if (all->tab != NULL)
-	{
-	while (all->tab[i])
-	{
-		free(all->tab[i]);
-		all->tab[i] = NULL;
-		i++;
-	}
-	free(all->tab);
-	}
-	all->tab = NULL;
-}
-
-int		stock_back(t_all *all)
-{
-	int i;
+	int i = 0;
 	int j;
-
-	i = 0;
-	if (!(all->tab = malloc(sizeof(char *) * ((int)all->back.height + 1))))
+	all->back = malloc(sizeof(char *) * ((int)all->b_height + 1));
+	if (all->back == NULL)
+		return(1);
+	all->back[(int)all->b_height] = NULL;
+	while (i < (int)all->b_height)
 	{
-		ft_putstr("malloc tab failed\n");
-		return (1);
-	}
-	all->tab[(int)all->back.height] = 0;
-	while (i < (int)all->back.height)
-	{
-		if (!(all->tab[i] = malloc(sizeof(char) * ((int)all->back.width + 1))))
-		{
-			ft_putstr("malloc tab[i] failed\n");
-			return (1);
-		}
 		j = 0;
-		while (j < (int)all->back.width)
+		all->back[i] = malloc(sizeof(char) * (all->b_width + 1));
+		if (all->back[i] == NULL)
+			return (1);
+		while (j < (int)all->b_width)
 		{
-			all->tab[i][j] = all->back.c;
+			all->back[i][j] = all->b_char;
 			j++;
 		}
-		all->tab[i][j] = '\0';
+		all->back[i][j] = '\0';
 		i++;
 	}
 	return (0);
 }
 
-int		check_and_stock_back(t_all *all)
+int		back(t_all *all)
 {
-	int ret;
 	char test;
-	ret = fscanf(all->file, "%f %f %c", &all->back.width, &all->back.height, &all->back.c);
-	if (ret != 3)
-	{
-		ft_putstr("Error: Operation file corrupted\n");
-		return(1);
-	}
+	if ((fscanf(all->file, "%f %f %c", &all->b_width, &all->b_height, &all->b_char)) != 3)
+		return(ft_putstr("Error: Operation file corrupted\n"));
 	fscanf(all->file, "%c", &test);
 	if (test != '\n' && test != '\0')
-	{
-		ft_putstr("Error: Operation file corrupted\n");
-		return(1);
-	}
-	if ((int)all->back.width < 0 || (int)all->back.height > 300 || (int)all->back.height < 0 || (int)all->back.height > 300)
-		return (1);
-	return(stock_back(all));
-}
-
-void	intizer(t_all* all, int *x, int *y, int *w, int *h)
-{
-	if (all->figure.x != (int)all->figure.x)
-	{
-		if ((int)all->figure.x >= 0)
-			*x = (int)all->figure.x + 1;
-		else
-			*x = (int)all->figure.x - 1;
-	}
-	else
-		*x = (int)all->figure.x;
-	if (all->figure.y != (int)all->figure.y)
-	{
-		if ((int)all->figure.y >= 0)
-			*y = (int)all->figure.y + 1;
-		else
-			*y = (int)all->figure.y - 1;
-	}
-	else
-		*y = (int)all->figure.y;
-	*w = all->figure.x + all->figure.width;
-	*h = all->figure.y + all->figure.height;
+		return(ft_putstr("Error: Operation file corrupted\n"));
+	if ((int)all->b_width <= 0 || (int)all->b_width > 300 || (int)all->b_height <= 0 || (int)all->b_height > 300)
+		return(ft_putstr("Error: Operation file corrupted\n"));
+	malloc_and_stock_back(all);
+	return (0);
 }
 /*
-void	add_figure(t_all *all)
+void	add_shape(t_all *all)
 {
-	int x;
-	int y;
-	int w;
-	int	h;
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
-	intizer(all, &x, &y, &w, &h);
-	i = y;
-	j = x;
-	printf("x %d\ny %d\nw %d\nh %d\ni %d\nj %d\n", x, y, w, h, i, j);
-	while (i <= h)
+	int bx = all->s_width + all->s_x;
+	int yx = all->s_height + all->syx;
+
+	while (all->back[i])
 	{
-		j = x;
-		while(j <= w)
+		while (all->back[i][j])
 		{
-			if (i < all->back.height && j < all->back.width && i >= 0 && j >= 0)
-			{
-				if (all->figure.r == 'r')
-				{
-					if (i == y || i == h || j == x || j == w)
-						all->tab[i][j] = all->figure.c;
-				}
-				else
-					all->tab[i][j] = all->figure.c;
-			}
+			if (i >= all->
 			j++;
 		}
 		i++;
 	}
 }
 */
-
-void	add_figure(t_all *all)
+int		shape(t_all *all)
 {
-	int i = 0;
-	int j = 0;
-	float	bx = all->figure.x + all->figure.width;
-	float	by = all->figure.y + all->figure.height;
-
-	while (i < all->back.height)
-	{
-		j = 0;
-		while (j < all->back.width)
-		{
-			if (i >= all->figure.y && i <= bx && j >= all->figure.x && j <= by)
-				all->tab[i][j] = all->figure.c;
-			j++;
-		}
-		i++;
-	}
-}
-
-int		figure(t_all *all)
-{
-	int ret;
 	char	test;
-	all->figure.r = 0;
-	ret = fscanf(all->file, "%c %f %f %f %f %c", &all->figure.r, &all->figure.x, &all->figure.y, &all->figure.width, &all->figure.height, &all->figure.c);
-	if (all->figure.r == '\0' || all->figure.r == ' ' || all->figure.r == '\n')
+	all->s_r = '\0';
+	if (((fscanf(all->file, "%c %f %f %f %f %c", &all->s_r,  &all->s_x, &all->s_y, &all->s_width, &all->s_height, &all->s_char)) != 6) && all->s_r != '\0')
+		return(ft_putstr("Error: Operation file corrupted\n"));
+	printf("%c %f %f %f %f %c\n", all->s_r,  all->s_x, all->s_y, all->s_width, all->s_height, all->s_char);
+	if (all->s_r == '\0')
 		return (2);
-	if (ret != 6)
-	{
-		ft_putstr("Error: Operation file corrupted\n");
-		return(1);
-	}
+	if (all->s_r != 'r' && all->s_r != 'R')
+		return(ft_putstr("Error: Operation file corrupted\n"));
 	fscanf(all->file, "%c", &test);
 	if (test != '\n' && test != '\0')
-	{
-		ft_putstr("Error: Operation file corrupted\n");
-		return(1);
-	}
-	if (test == '\0')
-		return(2);
-	add_figure(all);
+		return(ft_putstr("Error: Operation file corrupted\n"));
+//	add_shape(all);
 	return (0);
 }
 
-int 	main(int ac, char **av)
+int main(int ac, char **av)
 {
+	int ret = 5;
 	t_all all;
-	all.tab = NULL;
-
 	if (ac != 2)
-	{
-		ft_putstr("Error: argument\n");
-		return(1);
-	}
-	all.file = fopen(av[1], "r");
-	if (all.file == NULL)
-	{
-		ft_putstr("Error: Operation file corrupted\n");
-		return(1);
-	}
-	if (check_and_stock_back(&all) != 0)
-	{
-		ft_putstr("lol\n");
-		free_tab(&all);
-		fclose(all.file);
+		return (ft_putstr("Error: argument\n"));
+	if ((all.file = fopen(av[1], "r")) == NULL)
+		return(ft_putstr("Error: Operation file corrupted\n"));
+	if (back(&all) == 1)
 		return (1);
-	}
-	while (figure(&all) != 2)
-		;
 	print_tab(&all);
-	free_tab(&all);
-	fclose(all.file);
+	while (ret != 2)
+	{
+		ret = shape(&all);
+		if (ret == 1)
+		{
+			destroy(&all);
+			return(1);
+		}
+	}
+	destroy(&all);
 	return (0);
 }
---
+++ works.c
@@ -0,0 +1,266 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct	s_back
+{
+	float	width;
+	float	height;
+	char	c;
+}				t_back;
+
+typedef struct	s_figure
+{
+	char	r;
+	float	x;
+	float	y;
+	float	width;
+	float	height;
+	char	c;
+}				t_figure;
+
+typedef struct	s_all
+{
+	FILE *file;
+	char **tab;
+	t_back back;
+	t_figure figure;
+}				t_all;
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_putstr(char *str)
+{
+	write(1, str, ft_strlen(str));
+}
+void	print_tab(t_all *all)
+{
+	int i;
+
+	i = 0;
+	while (all->tab[i])
+	{
+		ft_putstr(all->tab[i]);
+		ft_putstr("\n");
+		i++;
+	}
+}
+
+void	free_tab(t_all *all)
+{
+	int i;
+
+	i = 0;
+	if (all->tab != NULL)
+	{
+	while (all->tab[i])
+	{
+		free(all->tab[i]);
+		all->tab[i] = NULL;
+		i++;
+	}
+	free(all->tab);
+	}
+	all->tab = NULL;
+}
+
+int		stock_back(t_all *all)
+{
+	int i;
+	int j;
+
+	i = 0;
+	if (!(all->tab = malloc(sizeof(char *) * ((int)all->back.height + 1))))
+	{
+		ft_putstr("malloc tab failed\n");
+		return (1);
+	}
+	all->tab[(int)all->back.height] = 0;
+	while (i < (int)all->back.height)
+	{
+		if (!(all->tab[i] = malloc(sizeof(char) * ((int)all->back.width + 1))))
+		{
+			ft_putstr("malloc tab[i] failed\n");
+			return (1);
+		}
+		j = 0;
+		while (j < (int)all->back.width)
+		{
+			all->tab[i][j] = all->back.c;
+			j++;
+		}
+		all->tab[i][j] = '\0';
+		i++;
+	}
+	return (0);
+}
+
+int		check_and_stock_back(t_all *all)
+{
+	int ret;
+	char test;
+	ret = fscanf(all->file, "%f %f %c", &all->back.width, &all->back.height, &all->back.c);
+	if (ret != 3)
+	{
+		ft_putstr("Error: Operation file corrupted\n");
+		return(1);
+	}
+	fscanf(all->file, "%c", &test);
+	if (test != '\n' && test != '\0')
+	{
+		ft_putstr("Error: Operation file corrupted\n");
+		return(1);
+	}
+	if ((int)all->back.width < 0 || (int)all->back.height > 300 || (int)all->back.height < 0 || (int)all->back.height > 300)
+		return (1);
+	return(stock_back(all));
+}
+
+void	intizer(t_all* all, int *x, int *y, int *w, int *h)
+{
+	if (all->figure.x != (int)all->figure.x)
+	{
+		if ((int)all->figure.x >= 0)
+			*x = (int)all->figure.x + 1;
+		else
+			*x = (int)all->figure.x - 1;
+	}
+	else
+		*x = (int)all->figure.x;
+	if (all->figure.y != (int)all->figure.y)
+	{
+		if ((int)all->figure.y >= 0)
+			*y = (int)all->figure.y + 1;
+		else
+			*y = (int)all->figure.y - 1;
+	}
+	else
+		*y = (int)all->figure.y;
+	*w = all->figure.x + all->figure.width;
+	*h = all->figure.y + all->figure.height;
+}
+/*
+void	add_figure(t_all *all)
+{
+	int x;
+	int y;
+	int w;
+	int	h;
+	int i;
+	int j;
+
+	intizer(all, &x, &y, &w, &h);
+	i = y;
+	j = x;
+	printf("x %d\ny %d\nw %d\nh %d\ni %d\nj %d\n", x, y, w, h, i, j);
+	while (i <= h)
+	{
+		j = x;
+		while(j <= w)
+		{
+			if (i < all->back.height && j < all->back.width && i >= 0 && j >= 0)
+			{
+				if (all->figure.r == 'r')
+				{
+					if (i == y || i == h || j == x || j == w)
+						all->tab[i][j] = all->figure.c;
+				}
+				else
+					all->tab[i][j] = all->figure.c;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+*/
+
+void	add_figure(t_all *all)
+{
+	int i = 0;
+	int j = 0;
+	float	bx = all->figure.x + all->figure.width;
+	float	by = all->figure.y + all->figure.height;
+
+	while (i < all->back.height)
+	{
+		j = 0;
+		while (j < all->back.width)
+		{
+			if (i >= all->figure.y && i <= by && j >= all->figure.x && j <= bx)
+			{
+				if (all->figure.r == 'r' && (i >= all->figure.y + 1 && i <= by -1  && j >= all->figure.x + 1 && j <= bx -1))
+					;
+				else
+				all->tab[i][j] = all->figure.c;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int		figure(t_all *all)
+{
+	int ret;
+	char	test;
+	all->figure.r = 0;
+	ret = fscanf(all->file, "%c %f %f %f %f %c", &all->figure.r, &all->figure.x, &all->figure.y, &all->figure.width, &all->figure.height, &all->figure.c);
+	if (all->figure.r == '\0' || all->figure.r == ' ' || all->figure.r == '\n')
+		return (2);
+	if (ret != 6)
+	{
+		ft_putstr("Error: Operation file corrupted\n");
+		return(1);
+	}
+	fscanf(all->file, "%c", &test);
+	if (test != '\n' && test != '\0')
+	{
+		ft_putstr("Error: Operation file corrupted\n");
+		return(1);
+	}
+	if (test == '\0')
+		return(2);
+	add_figure(all);
+	return (0);
+}
+
+int 	main(int ac, char **av)
+{
+	t_all all;
+	all.tab = NULL;
+
+	if (ac != 2)
+	{
+		ft_putstr("Error: argument\n");
+		return(1);
+	}
+	all.file = fopen(av[1], "r");
+	if (all.file == NULL)
+	{
+		ft_putstr("Error: Operation file corrupted\n");
+		return(1);
+	}
+	if (check_and_stock_back(&all) != 0)
+	{
+		ft_putstr("lol\n");
+		free_tab(&all);
+		fclose(all.file);
+		return (1);
+	}
+	while (figure(&all) != 2)
+		;
+	print_tab(&all);
+	free_tab(&all);
+	fclose(all.file);
+	return (0);
+}
